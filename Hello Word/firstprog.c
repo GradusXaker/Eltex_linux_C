aef50b4ca4c08120bc59038b43bcde24193398e7
Gradus <gradus_xaker@mail.ru>
2019-09-11 17:47:31 +0700

    Update and rename main.c to firstprog.c

---
+++ Hello Word/firstprog.c
@@ -0,0 +1,12 @@
+firstprog.c 
+#include <stdio.h> 
+int main() 
+{ 
+int i; 
+for(i=0; i < 10; i++) // Цикл повторяется 10 раз. 
+{ 
+printf(“Hello, world!\n”); // Вывести строку. 
+} 
+return 0; // Сообщить ОС, что программа 
+завершилась без ошибок. 
+}
